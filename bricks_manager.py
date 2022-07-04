c96072a6fd1b2ddf2bb5862cdbe66f0d7bc57a23
Paula <santkiewicz.paula@gmail.com>
2022-07-04 12:57:33 +0100

    write logic for bricks manager

--- bricks_manager.py
+++ bricks_manager.py
@@ -1,19 +1,36 @@
 from turtle import Turtle
 
+COLUMNS = 14
+ROWS = 8
 
-class Brick(Turtle):
 
+class BricksManager:
     def __init__(self):
         super().__init__()
-        pass
+        self.width = 2.5
+        self.height = 0.75
+        self.bricks_list = []
 
     def lay_bricks(self):
-        # 8 rows
-        # 14 columns
-        # from top: 2 rows red, 2 rows orange, 2 rows green and 2 rows yellow
-        pass
+        y_post = 50
+        x_post = -380
+        colors = ["yellow", "yellow", "green", "green", "orange", "orange", "red", "red"]
+        for row in range(ROWS):
+            color = colors[row]
+            for col in range(COLUMNS):
 
-    def destroy_brick(self):
+                y_pos = (self.width * 10) * row
+                x_pos = (self.height * 77) * col
 
-        # if no more bricks left: lay the brick again, level 2 begins
-        pass
+                brick = Turtle(shape="square")
+                brick.shapesize(stretch_wid=self.height, stretch_len=self.width)
+                brick.color(color)
+                brick.penup()
+                brick.goto(x_post + x_pos, y_post + y_pos)
+                self.bricks_list.append(brick)
+
+                # print(f"X: {row}: {x_post + x_pos} Y: {col}: {y_post + y_pos}")
+
+    def destroy_brick(self, brick):
+        self.bricks_list.remove(brick)
+
