3495c0beae8ad4428cac1815a181fbe328063616
Paula <santkiewicz.paula@gmail.com>
2022-07-04 18:04:21 +0100

    Fix bug with paddle collision

--- main.py
+++ main.py
@@ -6,20 +6,12 @@ from scoreboard import Scoreboard
 import time
 
 
-def game_over():
+def write_text(text):
     game_over_text = Turtle()
     game_over_text.color("blue")
     game_over_text.hideturtle()
     game_over_text.goto(0, -50)
-    game_over_text.write("GAME OVER", align="center", font=("Courier", 100, 'bold'))
-
-
-def you_won():
-    game_over_text = Turtle()
-    game_over_text.color("blue")
-    game_over_text.hideturtle()
-    game_over_text.goto(0, -50)
-    game_over_text.write("YOU WON!", align="center", font=("Courier", 100, 'bold'))
+    game_over_text.write(f"{text}", align="center", font=("Courier", 100, 'bold'))
 
 
 screen = Screen()
@@ -43,22 +35,28 @@ ball_hits = 0
 game_is_on = True
 orange_row_hit = False
 red_row_hit = False
-
-last_time = time.time()
-current_time = time.time()
+hit_brick = True
+last_time = time.time_ns()
 while game_is_on:
-    current_time = time.time()
-    delta = (current_time - last_time) * 1000.0
+    current_time = time.time_ns()
+    delta = (current_time - last_time) / 1000000.0
     last_time = current_time
     screen.update()
 
-    if ball_hits < 4:
-        ball.move(delta)
+    ball.move(delta)
+
+    ball_speed = 1.25
+    if 4 <= ball_hits < 12:
+        ball_speed += 0.25
+    if ball_hits >= 12:
+        ball_speed += 0.25
     if orange_row_hit:
-        ball.move(delta * 0.1)
+        ball_speed += 0.25
     if red_row_hit:
-        paddle.decrease_size_in_half()
-        ball.move(delta * 0.12)
+        ball_speed += 0.25
+
+    ball.set_speed(ball_speed)
+
     # Detect if ball collides with upper wall
     if ball.ycor() > 280:
         ball.bounce_y()
@@ -66,8 +64,9 @@ while game_is_on:
     if ball.xcor() > 380 or ball.xcor() < -380:
         ball.bounce_x()
     # Detect if ball collides with paddle and bounce:
-    if ball.distance(paddle) < 50 and ball.ycor() < -250:
+    if hit_brick and ball.distance(paddle) < 50 and ball.ycor() < -250:
         ball.bounce_y()
+        hit_brick = False
 
     # Detect if ball went past the paddle
     if ball.ycor() < -300:
@@ -76,19 +75,17 @@ while game_is_on:
         paddle.set_position()
         ball.set_position()
         if scoreboard.lives == 0:
-            game_over()
+            write_text("GAME OVER")
             game_is_on = False
 
     # Detect if ball collides with bricks, bounce and add points based on the brick color
     for brick in bricks_manager.bricks_list:
         if ball.distance(brick) < 25 and ball.xcor() > 0 or ball.distance(brick) < 25 and ball.xcor() < 0:
+            hit_brick = True
             brick.hideturtle()
             ball.bounce_y()
             bricks_manager.destroy_brick(brick)
             ball_hits += 1
-            print(delta)
-            print(current_time)
-            print(last_time)
             if brick.color() == ("yellow", "yellow"):
                 scoreboard.add_points(1)
             elif brick.color() == ("green", "green"):
@@ -100,15 +97,9 @@ while game_is_on:
                 scoreboard.add_points(7)
                 red_row_hit = True
 
-        if 4 <= ball_hits < 12:
-            ball.move(delta * 0.015)
-
-        elif ball_hits >= 12:
-            ball.move(delta * 0.02)
-
         # if no more bricks left - game is done, well done!
         if len(bricks_manager.bricks_list) == 0:
-            you_won()
+            write_text("YOU WON!")
             game_is_on = False
 
 screen.exitonclick()
